20f0d9f3631035b41deb342ca58224d14f5d2224
Tim Ellis <98844276+timmellis@users.noreply.github.com>
2022-09-22 16:59:34 -0400

    Time: 122 ms (58.77%), Space: 44.6 MB (44.25%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,15 +4,28 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    
     if(s.length !== t.length) return false
-    
-    sArr = s.split("").sort()
-    tArr = t.split("").sort()
-    
-    for(i=0; i<s.length; i++) {
-        if(sArr[i] !== tArr[i]) return false
-        // if(s.split("").sort()[i] !== t.split("").sort()[i]) return false
+
+    hash = {}
+    for(c of s) {
+        hash[c] = (hash[c] || 0) + 1
     }
-    return true   
+    for(c of t) {
+        if(!hash[c]) return false
+        else hash[c]--
+    }
+    return true
 };
+
+
+// var isAnagram = function(s, t) {
+//     if(s.length !== t.length) return false
+    
+//     sArr = s.split("").sort()
+//     tArr = t.split("").sort()
+
+//     for(i=0; i<s.length; i++) {
+//         if(sArr[i] !== tArr[i]) return false
+//     }
+//     return true   
+// };
