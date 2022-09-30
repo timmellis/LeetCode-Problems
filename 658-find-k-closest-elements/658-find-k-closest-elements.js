6f009bdb8db532b5f26078781576103a636e8e8e
Tim Ellis <98844276+timmellis@users.noreply.github.com>
2022-09-29 20:07:10 -0400

    Time: 155 ms (50.30%), Space: 47.9 MB (75.96%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.js
+++ 658-find-k-closest-elements/658-find-k-closest-elements.js
@@ -4,14 +4,43 @@
  * @param {number} x
  * @return {number[]}
  */
+
+var findClosestElements = function(arr, k, x) {
+    
+    if (x <= arr[0]) return arr.slice(0,k)
+    else if (x >= arr[arr.length-1]) return arr.slice(arr.length-k)
+    
+    
+    let left = 0, right = arr.length-k
+    
+    while(left < right) {
+        let mid = Math.floor((left + right) / 2)
+//         console.log("left:",left,"right:",right, "mid:",mid,"arr[mid]:",arr[mid])
+        
+        if (mid == left) {
+            if(Math.abs(arr[mid]-x) <= Math.abs((arr[mid+k])-x)) break;
+            else left++
+        }
+        else {
+            // console.log("compare dist of: ", arr[mid],"(",Math.abs(arr[mid]-x), ") vs:", arr[mid+k], "(", Math.abs(arr[mid+k]-x) , ")")
+            if(Math.abs(arr[mid]-x) <= Math.abs((arr[mid+k])-x) ) {
+                right = mid
+            }
+            else {
+                left = mid
+            }
+        }
+    }
+    return arr.slice(left, left+k)
+    
+    
+}
+
+/// WORK OUT FROM TARGET METHOD:
+/*
 var findClosestElements = function(arr, k, x) {
     let res = []
-    
-    // if(arr.includes(x)) {
-    //     res.push(x)
-    //     k--
-    // }
-    
+
     if (x <= arr[0]) res = arr.slice(0,k)
     else if (x >= arr[arr.length-1]) res = arr.slice(arr.length-k)
     
@@ -49,3 +78,4 @@ var findClosestElements = function(arr, k, x) {
         
     return res
 };
+*/
